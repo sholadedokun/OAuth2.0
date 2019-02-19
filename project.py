8efd2ae64875a4d79fb0ea83188fc48aac666077
Olushola <sholadedokun@gmail.com>
2019-02-19 03:58:04 +0100

    Added user helper function

--- project.py
+++ project.py
@@ -145,6 +145,30 @@ def gdisconnect():
         return response
 
 
+# User Helper Functions
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
@@ -179,7 +203,7 @@ def newRestaurant():
        return redirect('/login')
     if request.method == 'POST':
 
-        newRestaurant = Restaurant(name = request.form['name'])
+        newRestaurant = Restaurant(name = request.form['name'], user_id = login_session['user_id'])
         session.add(newRestaurant)
         flash('New Restaurant %s Successfully Created' % newRestaurant.name)
         session.commit()
@@ -233,7 +257,7 @@ def newMenuItem(restaurant_id):
        return redirect('/login')
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     if request.method == 'POST':
-        newItem = MenuItem(name = request.form['name'], description = request.form['description'], price = request.form  ['price'], course = request.form['course'], restaurant_id = restaurant_id)
+        newItem = MenuItem(name = request.form['name'], description = request.form['description'], price = request.form  ['price'], course = request.form['course'], restaurant_id = restaurant_id, user_id = restaurant.user_id)
         session.add(newItem)
         session.commit()
         flash('New Menu %s Item Successfully Created' % (newItem.name))
